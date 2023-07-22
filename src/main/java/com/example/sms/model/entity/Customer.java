232b1558ad7e37a13bbacb3e98ddf6cba5406a5d
Emilmacbooku <emil.aliyev@div.edu.az>
2023-07-22 19:35:38 +0400

    Customer

--- src/main/java/com/example/sms/model/entity/Customer.java
+++ src/main/java/com/example/sms/model/entity/Customer.java
@@ -3,6 +3,10 @@ package com.example.sms.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -14,8 +18,29 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
-    private String name;
-    private String surname;
+
+    @Pattern(regexp = "\\d+")
+    @Min(value = 18, message = "Your age must be over 18")
+    @Column(name = "customer_age", nullable = false, length = 2)
     private int age;
-    private long
+    @Column(name = "customer_status")
+    private int status;
+    @Pattern(regexp = "[A-Za-z]+")
+    @Column(name = "customer_name", nullable = false, length = 25)
+    private String name;
+    @Pattern(regexp = "[A-Za-z]+")
+    @Column(name = "customer_surname", nullable = false, length = 25)
+    private String surname;
+    @Pattern(regexp = "\\+\\d+")
+    @Column(name = "customer_phone", nullable = false)
+    private String phone;
+    @Pattern(regexp = "[A-Za-z0-9]+")
+    @Column(name = "customer_adress", nullable = false, length = 100)
+    private String adress;
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+    @Column(name = "customer_email", nullable = false, length = 20)
+    private String email;
+    @Column(name = "customer_registration_date")
+    private LocalDateTime registrationDate;
+
 }
