021603cc3928d71c7e0363a5551126aad3c26990
Emilmacbooku <emil.aliyev@div.edu.az>
2023-07-22 20:29:13 +0400

    Order

---
+++ src/main/java/com/example/sms/model/entity/Order.java
@@ -0,0 +1,34 @@
+package com.example.sms.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Table(name = "Order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "order_status")
+    private int status;
+    @Pattern(regexp = "[A-Za-z0-9]+")
+    @Column(name = "order_weight", nullable = false, length = 25)
+    private Double weight;
+    @Pattern(regexp = "[A-Za-z0-9]+")
+    @Column(name = "order_price", nullable = false, length = 25)
+    private int price;
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+    @Pattern(regexp = "[A-Za-z0-9]+")
+    @Column(name = "order_shipping_adress", nullable = false, length = 30)
+    private String shippingAdress;
+}
